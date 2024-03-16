27d853d32c469fd225f1fb5bbcd8fe0505606c9d
Aditya Bhargava <adit@adit.io>
2024-03-16 18:22:39 -0500

    parser tests

---
+++ lib/parsers.test.ts
@@ -0,0 +1,69 @@
+import { describe, expect, it } from "vitest";
+import { char, str } from "./parsers";
+
+function success({ match, rest }: { match: string; rest: string }) {
+  return { success: true, match, rest };
+}
+
+function failure({ rest, message }: { rest: string; message: string }) {
+  return { success: false, rest, message };
+}
+
+describe("char parser", () => {
+  it("should parse correct character", () => {
+    const parser = char("a");
+    expect(parser("abc")).toEqual(success({ match: "a", rest: "bc" }));
+  });
+
+  it("should handle unexpected end of input", () => {
+    const parser = char("a");
+    expect(parser("")).toEqual(
+      failure({
+        rest: "",
+        message: "unexpected end of input",
+      })
+    );
+  });
+
+  it("should handle incorrect character", () => {
+    const parser = char("a");
+    expect(parser("bcd")).toEqual(
+      failure({
+        rest: "bcd",
+        message: "expected a, got b",
+      })
+    );
+  });
+});
+
+describe("str parser", () => {
+  it("should parse correct string", () => {
+    const parser = str("hello");
+    expect(parser("hello world")).toEqual(
+      success({
+        match: "hello",
+        rest: " world",
+      })
+    );
+  });
+
+  it("should handle incorrect string", () => {
+    const parser = str("hello");
+    expect(parser("hi there")).toEqual(
+      failure({
+        rest: "i there",
+        message: "expected e, got i",
+      })
+    );
+  });
+
+  it("should handle unexpected end of input", () => {
+    const parser = str("hello");
+    expect(parser("hel")).toEqual(
+      failure({
+        rest: "",
+        message: "unexpected end of input",
+      })
+    );
+  });
+});
