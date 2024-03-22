9fbf723969ed3093e5d09dcf40a7b0d045856941
Aditya Bhargava <adit@adit.io>
2024-03-22 18:35:32 -0500

    cake

--- index.ts
+++ index.ts
@@ -8,21 +8,6 @@ import {
 } from "./lib/combinators.js";
 import { char, eof, space, str } from "./lib/parsers.js";
 import { createTree } from "./lib/types.js";
-/* const parser = seq(
-  [
-    str("the robot"),
-    space,
-    str("ate"),
-    space,
-    or([str("the"), str("the cake")]),
-    capture(optional(str(" pie")), "food"),
-  ],
-  getResults
-);
-// without needing to backtrack
-const resultPie = parser("the robot ate the pie");
-console.log(resultPie);
- */
 
 const parser = seq(
   [
@@ -30,37 +15,15 @@ const parser = seq(
     space,
     str("ate"),
     space,
-    or([str("the"), str("the cake")]),
-    optional(str(" pie")),
+    or([str("the"), str("the cake-")]),
+    space,
+    or([str("cake"), str("cake cake")]),
+    str("!"),
     eof,
   ],
   getResults
 );
-// without needing to backtrack
-const resultCake = parser("the robot ate the cake");
+
+const resultCake = parser("the robot ate the cake- cake!");
+
 console.log(resultCake);
-/* 
-const tree = createTree([
-  str("the robot"),
-  space,
-  str("ate"),
-  space,
-  or([str("the"), str("the cake")]),
-  optional(str(" pie")),
-]);
-
-console.log(tree);
- */
-
-/* const a = char("a");
-const b = char("b");
-const c = char("c");
-const one = or([seq([a, c], getResults), seq([b, c], getResults)]);
-const two = seq([or([a, b]), c], getResults);
-
-const result1 = one("ac");
-const result2 = one("bc");
-const result3 = two("ac");
-const result4 = two("bc");
-console.log({ result1, result2, result3, result4 });
- */
